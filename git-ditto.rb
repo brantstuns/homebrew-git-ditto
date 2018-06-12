8ea1cc3fb8173e546b3a5c1125259d386b9bdde0
Brant Stuns <brantstuns@gmail.com>
2018-06-12 00:26:21 -0700

    update formula

--- git-ditto.rb
+++ git-ditto.rb
@@ -2,7 +2,7 @@ class GitDitto < Formula
   desc "A tool to quickly add a mirrored repo to your project so you can start easily pushing two places at once 🤝"
   homepage "https://github.com/brantstuns/git-ditto"
   url "https://github.com/brantstuns/git-ditto/archive/v1.0.1.tar.gz"
-  sha256 "c5bc92fc292bbcea821099d5a09ef497ae101184ef90dbc2abbefea76461b104"
+  sha256 "deb726f5eba806970cd538aa62de7946023d6477d845ae44727425366e7aba5f"
 
   def install
     bin.install 'bin/git-ditto'
